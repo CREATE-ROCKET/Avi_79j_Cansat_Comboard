32363d51c8980a61ec852f4578b0a6cc259d3375
katsu <katsuki10632@gmail.com>
2024-03-25 01:45:06 +0900

    ディレイの追加

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,21 @@ SoftwareSerial mySerial(3,1); //RX, TX
 
 void setup(){
     Serial.begin(9600, SERIAL_8N1, UART_RXD, UART_TXD);     // gps
-    Serial2.begin(115200, SERIAL_8N1, UART_RX1, UART_TX1);  // teilite1
-    Serial1.begin(115200, SERIAL_8N1, UART_RX2, UART_TX2);  // teilite2
+    Serial1.begin(115200, SERIAL_8N1, UART_RX2, UART_TX2);  // 本部
+    Serial2.begin(115200, SERIAL_8N1, UART_RX1, UART_TX1);  // 本体　    
     mySerial.begin(115200);
+    delay(1000);
 }
 
 
 // ログ基盤に缶サットのGPSを送る
 void loop(){
     if (Serial.available()){ 
-        char j = Serial.read();
-        //                                                                                                                                                Serial1.write(j);
+        char j = Serial.read();                                                                                                                                               Serial1.write(j);
         gpsreceiveC[count]=j;
         count++;
         if(count>79 || j==0x0A){
-            Serial1.print("CansatGPS:");
+            Serial1.print("CansatGPS　:");
             Serial1.write(gpsreceiveC,count);
             mySerial.write(gpsreceiveC,count);
             count=0;
@@ -46,8 +46,6 @@ void loop(){
         gpsreceiveA[count1]=rcmd;
         count1++;
         if(count1>79 || rcmd==0x0A){
-           // Serial2.print("receive2");
-           // Serial2.write(gpsreceiveA,count);
             Serial1.print("GPS from Hontai :");
             Serial1.write(gpsreceiveA,count1);
             count1=0;
@@ -57,7 +55,7 @@ void loop(){
         }
     }
 
-//　ログ基盤に本体のkomanndoを送る
+//　ログ基盤に本部のkomanndoを送る
     if (Serial1.available()){
         char cmd = Serial1.read();
         Serial1.println(cmd);
@@ -65,7 +63,6 @@ void loop(){
             Serial.write("$PMTK161,0*28\r\n");
         }else if(cmd =='g'){
             Serial.write("$PMTK161,1*29\r\n");
-            
         }else{
             mySerial.write(cmd);
         }
